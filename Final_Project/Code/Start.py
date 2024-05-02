22ad9ae7c86e00f5e92138d23a69aa50a4f09257
Joey Coco <baronlegend27@gmail.com>
2024-05-02 12:19:41 -0500

    WOrks but does not do anything

--- Final_Project/Code/Start.py
+++ Final_Project/Code/Start.py
@@ -1,10 +1,1 @@
-import os
-
-# Check if the environment variable "NAME" is set
-name = os.getenv("NAME")
-
-# If the environment variable is not set, prompt the user for input
-if name is None:
-    name = input("What is your name? ")
-
-print("Hello,", name)
+print("Hello")
