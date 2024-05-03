f420ead2cdfeabf166385ecec4c54fd48b7924ab
Tony7hang <123335079+Tony7hang@users.noreply.github.com>
2024-05-02 19:57:07 -0500

    Update ML.py

--- Final_Project/Code/ML.py
+++ Final_Project/Code/ML.py
@@ -135,6 +135,6 @@ df_filled['guess'] = (df_filled.iloc[:, 2] > 0.5).astype(int)
 percentage = (df_filled['guess'] == df_filled['result']).mean() * 100
 
 df_acc = df_filled[['output_id', 'result', 'guess']]
-df_filled.to_csv('testAcc.csv', index = False)
+df_acc.to_csv('testAcc.csv', index = False)
 
 print("\nAccuracy (Percentage of time when predicted matched actual label): {}%\n".format(percentage))
